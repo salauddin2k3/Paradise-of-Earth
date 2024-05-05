44da5887fd2e5cabcb426c22e225666d194f8a4f
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-05-05 22:03:56 +0600

    added extra 2 section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import Estates from "./Estates";
 import Slider from "./Slider";
+import Contact from "./Contact";
+import Faq from "./Faq";
 
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
             <Helmet><title>Paradise of Earth</title></Helmet>
             <Slider></Slider>
             <Estates></Estates>
+            <Faq></Faq>
+            <Contact></Contact>
         </div>
     );
 };
