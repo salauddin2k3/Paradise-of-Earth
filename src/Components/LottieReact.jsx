89407f52e698d7ef6e758b213a87a595d82230ae
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-05-09 03:57:31 +0600

    applied typewriter

--- src/Components/LottieReact.jsx
+++ src/Components/LottieReact.jsx
@@ -1,11 +1,22 @@
 import Lottie from "lottie-react";
 import MyLottie from "../../public/lottie-react.json"
+import { Typewriter } from "react-simple-typewriter";
 
 
 const LottieReact = () => {
     return (
         <div className="mt-28 border border-gray-300 p-7 rounded-xl">
-            <h2 className="font-bold text-center text-4xl">{`Let's conquer the world.......`}</h2>
+            <div>
+                <h2 className="border border-gray-200 rounded-full p-3 font-bold text-center text-4xl">{`Let's`} <span className="text-secondary"><Typewriter
+                    words={['explore the world', 'discover the world', 'journey through the world', 'traverse the world', 'adventure around the world', 'wander the world']}
+                    loop={true}
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={120}
+                    deleteSpeed={80}
+                    delaySpeed={1000}
+                ></Typewriter></span></h2>
+            </div>
             <div className="mt-10 w-full">
                 <Lottie
                     animationData={MyLottie}
